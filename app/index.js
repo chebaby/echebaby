88c72b2b087329a81ee6f71fe0cc9a6c3d0e9f5a
Nour-Eddine ECH-CHEBABY <ech.chebaby@outlook.com>
2018-08-13 00:56:32 +0100

    First push

---
+++ app/index.js
@@ -0,0 +1,36 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+
+require('scripts/jquery-3.2.1.min');
+
+require('scripts/bootstrap.min');
+
+require('scripts/jquery.magnific-popup');
+
+require('scripts/owl.carousel');
+
+require('scripts/wow.min');
+
+require('scripts/owl.carousel');
+
+require('scripts/typed');
+
+require('scripts/particles');
+
+require('scripts/app');
+
+require('scripts/validator');
+
+require('scripts/contact');
+
+require('scripts/main');
+
+
