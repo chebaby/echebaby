db82f9faa89b8a575976b490aea23db9efa2cf11
Nour-Eddine ECH-CHEBABY <ech.chebaby@gmail.com>
2018-10-25 18:51:31 +0100

    add portfolio images

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -22,14 +22,14 @@ $(function () {
                 data: $(this).serialize(),
                 success: function (data)
                 {
-                    // data = JSON object that contact.php returns
-
+                    var data         = JSON.parse(data);
+                    
                     // we recieve the type of the message: success x danger and apply it to the 
                     var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
+                    var messageText  = data.message;
+                    
                     // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    var alertBox     = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     
                     // If we have messageAlert and messageText
                     if (messageAlert && messageText) {
